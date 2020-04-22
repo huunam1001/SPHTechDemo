9a6ccccb72a0281b9c27bd5e0c001cade17e3c4f
ninh nam <huunam101@gmail.com>
2020-04-22 08:46:48 +0700

    First commit to git

---
+++ SphTech/SphTech/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  SphTech
+//
+//  Created by ninh nam on 4/22/20.
+//  Copyright © 2020 ninh nam. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
