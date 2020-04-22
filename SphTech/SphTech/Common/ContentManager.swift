ab62e1a5b616b5dba6165453de547b2ecd1098ad
ninh nam <huunam101@gmail.com>
2020-04-22 08:52:08 +0700

    Add ContentManger class to control business of app

---
+++ SphTech/SphTech/Common/ContentManager.swift
@@ -0,0 +1,16 @@
+//
+//  ContentManager.swift
+//  SphTech
+//
+//  Created by ninh nam on 4/22/20.
+//  Copyright © 2020 ninh nam. All rights reserved.
+//
+
+import UIKit
+
+class ContentManager: NSObject {
+
+    // MARK:- Singleton
+    
+    static let shareManager = ContentManager()
+}
